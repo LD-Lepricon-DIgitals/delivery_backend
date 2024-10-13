294e6e6d04800218ee10e8867dd9741b7d2a5b3a
FUZI <k.lipovok@gmail.com>
2024-10-13 19:42:15 +0300

    AddDish test update

--- pkg/db/dishService_test.go
+++ pkg/db/dishService_test.go
@@ -17,16 +17,53 @@ func TestDishService_AddDish(t *testing.T) {
 	sqlxDB := sqlx.NewDb(db, "sqlmock")
 	dishService := dbServ.NewDishService(sqlxDB)
 
-	mock.ExpectQuery("INSERT INTO dishes").
-		WithArgs("dish_name_test", 100.0, 100.1, "dish_description_test", "dish_photo_test").
-		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+	type args struct {
+		dishName        string
+		dishPrice       float64
+		dishWeight      float64
+		dishDescription string
+		dishPhoto       string
+	}
+	type mockBehavior func(args args)
 
-	id, err := dishService.AddDish("dish_name_test", 100.0, 100.1, "dish_description_test", "dish_photo_test")
-	assert.NoError(t, err)
-	assert.Equal(t, 1, id)
-
-	err = mock.ExpectationsWereMet()
-	assert.NoError(t, err)
+	testTable := []struct {
+		name         string
+		mockBehavior mockBehavior
+		args         args
+		id           int
+		wantErr      bool
+	}{
+		{
+			name: "OK",
+			args: args{
+				dishName:        "test",
+				dishPrice:       10.1,
+				dishWeight:      100.0,
+				dishDescription: "test",
+				dishPhoto:       "url",
+			},
+			id: 1,
+			mockBehavior: func(args args) {
+				mock.ExpectQuery("INSERT INTO dishes").
+					WithArgs(args.dishName, args.dishPrice, args.dishWeight, args.dishDescription, args.dishPhoto).
+					WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+			},
+		},
+	}
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			testCase.mockBehavior(testCase.args)
+			got, err := dishService.AddDish(testCase.args.dishName, testCase.args.dishPrice, testCase.args.dishWeight, testCase.args.dishDescription, testCase.args.dishPhoto)
+			if testCase.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, testCase.id, got)
+				err = mock.ExpectationsWereMet()
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
 func TestDishService_DeleteDish(t *testing.T) {
 	db, mock, err := sqlmock.New()
