e137ddd3ec01e90ebceadd2ed5ad8ad10500137c
DrLivsey00 <emihrin50@gmail.com>
2024-10-15 23:06:08 +0300

    simple tests

--- pkg/db/userService_test.go
+++ pkg/db/userService_test.go
@@ -1,15 +1,14 @@
 package db_test
 
 import (
+	"fmt"
 	"github.com/DATA-DOG/go-sqlmock"
 	dbServ "github.com/LD-Lepricon-DIgitals/delivery_backend/pkg/db"
 	"github.com/jmoiron/sqlx"
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestUserService_CreateUser(t *testing.T) {
-
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
@@ -19,27 +18,76 @@ func TestUserService_CreateUser(t *testing.T) {
 
 	userService := dbServ.NewUserService(sqlxDB)
 
-	mock.ExpectBegin()
-	mock.ExpectQuery("INSERT INTO users").
-		WithArgs("login_test", "hashed_password_test").
-		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
-	mock.ExpectExec("INSERT INTO users_info").
-		WithArgs(1, "123456789", "John", "Doe", "123 Test St").
-		WillReturnResult(sqlmock.NewResult(1, 1))
+	type args struct {
+		login, name, surname, address, phoneNumber, password string
+	}
 
-	mock.ExpectCommit()
+	tests := []struct {
+		name    string
+		input   args
+		mock    func(input args)
+		wantErr bool
+	}{
+		{
+			name: "Ok",
+			input: args{
+				login:       "test login",
+				name:        "test name",
+				surname:     "test surname",
+				address:     "test address",
+				phoneNumber: "123456789",
+				password:    "test password",
+			},
+			mock: func(input args) {
+				mock.ExpectBegin()
+				mock.ExpectQuery("INSERT INTO users").
+					WithArgs(input.login, input.password).
+					WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+				mock.ExpectExec("INSERT INTO users_info").
+					WithArgs(1, input.phoneNumber, input.name, input.surname, input.address).
+					WillReturnResult(sqlmock.NewResult(1, 1))
+				mock.ExpectCommit()
+			},
+			wantErr: false,
+		},
+		{
+			name: "Fail to insert into users",
+			input: args{
+				login:       "test login",
+				name:        "test name",
+				surname:     "test surname",
+				address:     "test address",
+				phoneNumber: "123456789",
+				password:    "test password",
+			},
+			mock: func(input args) {
+				mock.ExpectBegin()
+				mock.ExpectQuery("INSERT INTO users").
+					WithArgs(input.login, input.password).
+					WillReturnError(fmt.Errorf("failed to insert into users"))
+				mock.ExpectRollback()
+			},
+			wantErr: true,
+		},
+	}
 
-	userId, err := userService.CreateUser("login_test", "John", "Doe", "123 Test St", "123456789", "hashed_password_test")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mock(tt.input)
+			_, err := userService.CreateUser(tt.input.login, tt.input.name, tt.input.surname, tt.input.address, tt.input.phoneNumber, tt.input.password)
 
-	assert.NoError(t, err)
-	assert.Equal(t, 1, userId)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CreateUser() error = %v, wantErr %v", err, tt.wantErr)
+			}
 
-	err = mock.ExpectationsWereMet()
-	assert.NoError(t, err)
-
+			if err := mock.ExpectationsWereMet(); err != nil {
+				t.Errorf("there were unfulfilled expectations: %v", err)
+			}
+		})
+	}
 }
 
-func TestUserService_ChangeUserCredentials(t *testing.T) {
+func TestUserService_ChangePassword(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
@@ -49,18 +97,77 @@ func TestUserService_ChangeUserCredentials(t *testing.T) {
 	sqlxDB := sqlx.NewDb(db, "sqlmock")
 	userService := dbServ.NewUserService(sqlxDB)
 
-	mock.ExpectBegin()
-	mock.ExpectExec("UPDATE users SET user_login = \\$1 WHERE id = \\$2").
-		WithArgs("new_login", 1).
-		WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectExec("UPDATE users_info SET user_name = \\$1, user_surname = \\$2, user_adress = \\$3 WHERE user_id = \\$4").
-		WithArgs("John", "Doe", "123 Test St", 1).
-		WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectCommit()
+	tests := []struct {
+		name    string
+		userID  int
+		newPass string
+		mock    func(userID int, newPass string)
+		wantErr bool
+	}{
+		{
+			name:    "Ok",
+			userID:  1,
+			newPass: "new_password",
+			mock: func(userID int, newPass string) {
+				mock.ExpectBegin()
+				mock.ExpectExec("UPDATE users SET user_hashed_password = \\$1 WHERE id = \\$2").
+					WithArgs(newPass, userID).
+					WillReturnResult(sqlmock.NewResult(1, 1))
+				mock.ExpectCommit()
+			},
+			wantErr: false,
+		},
+		{
+			name:    "User not found",
+			userID:  2,
+			newPass: "new_password",
+			mock: func(userID int, newPass string) {
+				mock.ExpectBegin()
+				mock.ExpectExec("UPDATE users SET user_hashed_password = \\$1 WHERE id = \\$2").
+					WithArgs(newPass, userID).
+					WillReturnResult(sqlmock.NewResult(0, 0))
+				mock.ExpectRollback()
+			},
+			wantErr: true,
+		},
+		{
+			name:    "Error during update",
+			userID:  3,
+			newPass: "new_password",
+			mock: func(userID int, newPass string) {
+				mock.ExpectBegin()
+				mock.ExpectExec("UPDATE users SET user_hashed_password = \\$1 WHERE id = \\$2").
+					WithArgs(newPass, userID).
+					WillReturnError(fmt.Errorf("some error"))
+				mock.ExpectRollback()
+			},
+			wantErr: true,
+		},
+		{
+			name:    "Error during commit",
+			userID:  4,
+			newPass: "new_password",
+			mock: func(userID int, newPass string) {
+				mock.ExpectBegin()
+				mock.ExpectExec("UPDATE users SET user_hashed_password = \\$1 WHERE id = \\$2").
+					WithArgs(newPass, userID).
+					WillReturnResult(sqlmock.NewResult(1, 1))
+				mock.ExpectCommit().WillReturnError(fmt.Errorf("commit error"))
+			},
+			wantErr: true,
+		},
+	}
 
-	err = userService.ChangeUserCredentials(1, "new_login", "John", "Doe", "123 Test St", "123456789")
-	assert.NoError(t, err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mock(tt.userID, tt.newPass)
+			err := userService.ChangePassword(tt.userID, tt.newPass)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ChangePassword() error = %v, wantErr %v", err, tt.wantErr)
+			}
 
-	err = mock.ExpectationsWereMet()
-	assert.NoError(t, err)
+			if err := mock.ExpectationsWereMet(); err != nil {
+			}
+		})
+	}
 }
