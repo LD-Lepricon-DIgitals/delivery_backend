5118a5bad4298486bc172af6aada23a73d453478
FUZI <k.lipovok@gmail.com>
2024-10-17 20:57:15 +0300

    Dish service fix

--- internal/handlers/dishes.go
+++ internal/handlers/dishes.go
@@ -5,14 +5,10 @@ import (
 	"strconv"
 )
 
-func respondWithError(ctx fiber.Ctx, statusCode int, message string) error {
-	return ctx.Status(statusCode).JSON(fiber.Map{"error": message})
-}
-
 func (h *Handlers) GetDishes(ctx fiber.Ctx) error {
 	dishes, err := h.services.GetDishes()
 	if err != nil {
-		return respondWithError(ctx, fiber.StatusBadRequest, "Failed to get dishes")
+		return fiber.NewError(fiber.StatusBadRequest, "Failed to get dishes")
 	}
 	return ctx.Status(fiber.StatusOK).JSON(dishes)
 }
@@ -30,11 +26,11 @@ func (h *Handlers) AddDish(ctx fiber.Ctx) error {
 	var payload AddDishPayload
 	err := ctx.Bind().Body(&payload)
 	if err != nil {
-		return respondWithError(ctx, fiber.StatusBadRequest, "Invalid request body")
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid request body")
 	}
 	id, err := h.services.AddDish(payload.Name, payload.Price, payload.Weight, payload.Description, payload.Photo, payload.Category)
 	if err != nil {
-		return respondWithError(ctx, fiber.StatusBadRequest, "Failed to add dish")
+		return fiber.NewError(fiber.StatusInternalServerError, "Failed to add dish")
 	}
 	return ctx.Status(fiber.StatusCreated).JSON(fiber.Map{
 		"id": id,
@@ -46,7 +42,7 @@ func (h *Handlers) DeleteDish(ctx fiber.Ctx) error {
 	id, err := strconv.Atoi(dishId)
 	err = h.services.DeleteDish(id)
 	if err != nil {
-		return respondWithError(ctx, fiber.StatusInternalServerError, "Failed to delete dish by id")
+		return fiber.NewError(fiber.StatusInternalServerError, "Failed to delete dish by id")
 	}
 	return ctx.SendStatus(fiber.StatusOK)
 }
@@ -65,11 +61,11 @@ func (h *Handlers) ChangeDish(ctx fiber.Ctx) error {
 	var payload ChangeDishPayload
 	err := ctx.Bind().Body(&payload)
 	if err != nil {
-		return respondWithError(ctx, fiber.StatusBadRequest, "Invalid request body")
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid request body")
 	}
 	err = h.services.ChangeDish(payload.Id, payload.Name, payload.Price, payload.Weight, payload.Description, payload.Photo, payload.Category)
 	if err != nil {
-		return respondWithError(ctx, fiber.StatusInternalServerError, "Failed to change dish")
+		return fiber.NewError(fiber.StatusInternalServerError, "Failed to change dish")
 	}
 	return ctx.SendStatus(fiber.StatusOK)
 }
@@ -82,11 +78,11 @@ func (h *Handlers) GetDishesByCategory(ctx fiber.Ctx) error {
 	var payload GetDishesByCategoryPayload
 	err := ctx.Bind().Body(&payload)
 	if err != nil {
-		return respondWithError(ctx, fiber.StatusBadRequest, "Invalid request body")
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid request body")
 	}
 	dishes, err := h.services.GetDishesByCategory(payload.Category)
 	if err != nil {
-		return respondWithError(ctx, fiber.StatusInternalServerError, "Failed to get dishes by category")
+		return fiber.NewError(fiber.StatusInternalServerError, "Failed to get dishes by category")
 	}
 	return ctx.Status(fiber.StatusOK).JSON(dishes)
 }
@@ -95,11 +91,11 @@ func (h *Handlers) GetDishById(ctx fiber.Ctx) error {
 	dishId := ctx.Params("dish_id")
 	id, err := strconv.Atoi(dishId)
 	if err != nil {
-		return respondWithError(ctx, fiber.StatusBadRequest, "Invalid parameter 'dish_id'")
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid parameter 'dish_id'")
 	}
 	dish, err := h.services.GetDishById(id)
 	if err != nil {
-		return respondWithError(ctx, fiber.StatusInternalServerError, "Failed to get dish by id")
+		return fiber.NewError(fiber.StatusInternalServerError, "Failed to get dish by id")
 	}
 	return ctx.Status(fiber.StatusOK).JSON(dish)
 }
@@ -107,11 +103,14 @@ func (h *Handlers) GetDishById(ctx fiber.Ctx) error {
 func (h *Handlers) SearchByName(ctx fiber.Ctx) error {
 	name := ctx.Query("name")
 	if name == "" {
-		return respondWithError(ctx, fiber.StatusBadRequest, "Query parameter 'name' is required")
+		return fiber.NewError(fiber.StatusBadRequest, "Query parameter 'name' is required")
 	}
 	dishes, err := h.services.SearchByName(name)
 	if err != nil {
-		return respondWithError(ctx, fiber.StatusInternalServerError, "Failed to search by name")
+		return fiber.NewError(fiber.StatusInternalServerError, "Failed to search by name")
+	}
+	if dishes == nil {
+		return fiber.NewError(fiber.StatusNotFound, "Dish not found")
 	}
 	return ctx.Status(fiber.StatusOK).JSON(dishes)
 }
